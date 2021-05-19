dd1762528c28f21626f834b90b4873382b87944e
Rimac48 <mattia.bertozzi@live.it>
2021-05-19 11:07:21 +0200

    Primo Commit

---
+++ Bertozzi.Mattia.4H.Levensthiengiusto/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Bertozzi.Mattia._4H.Levensthiengiusto
+{
+    class Program
+    {
+        static public string publicS { get; set; }
+        static public string publicT { get; set; }
+
+        static void Main(string[] args)
+        {
+            string s = "naso";
+            publicS = s;
+            string t = "casa";
+            publicT = t;
+
+            Console.WriteLine($"\nla distanza di Levenshtein tra '{s}' e '{t}' è {DistanzaLevenshtein(s,t)}");
+        }
+
+        static int DistanzaLevenshtein(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+
+            //punto 2.1
+            if (n==0)
+                return m;
+            //punto 2.2
+            if (m == 0)
+                return n;
+
+            //inizializzare la matrice m+1 righe,n+1 colonne
+            int[,] distanze = new int[m+1,n+1];
+
+            //punto 3.1
+            //riga 01234
+            for (int i = 0; i < n+1; i++)
+            {
+                distanze[0, i] = i;
+            }
+            //punto 3.2
+            //colonna 01234
+            for (int j = 0; j < m+1; j++)
+            {
+                distanze[j, 0] = j;
+            }
+
+            //extra
+            //StampaMatrice(distanze);
+            Console.WriteLine("\n\n");
+
+            //punto 4
+            //i righe
+            for (int i = 0; i < n; i++)
+            {
+                //j colonne
+                for (int j = 0; j < m; j++)
+                {
+                    //punto 4.1
+                    //int costo = (s[i]==t[j]) ? 0 : 1;
+                    //t[j] contiene il carattere della parola nella riga
+                    //s[i] contiene il carattere della parola nella colonna
+
+                    int costo;
+                    if (t[j] == s[i])
+                        costo = 0;
+                    else
+                        costo = 1;
+
+                    //punto 4.2
+                    distanze[j + 1, i + 1] = Minimo(distanze[j+1,i]+1, distanze[j,i+1]+1, distanze[j,i]+costo);
+                }
+            }
+
+            StampaMatrice(distanze);
+            //punto 5
+            return distanze[m,n];
+        }
+        static int Minimo(int a,int b,int c)
+        {
+            int ret = a;
+            if (b < ret)
+                ret= b;
+            if (c < ret)
+                ret= c;
+
+            return ret;
+        }
+
+        static void StampaMatrice(int[,] distanze)
+        {
+            for (int i = 0; i < publicS.Length+1; i++)
+            {
+                //Console.WriteLine("\n");
+                for (int j = 0; j < publicT.Length+1; j++)
+                    Console.Write(distanze[i, j]);
+                    Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Vecchio/Bertozzi.Mattia.4H.Levensthien/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Bertozzi.Mattia._4H.Levensthien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //cambiare le parole per prove diverse
+            //1.
+            Console.WriteLine($"Costante: {DistanzaLevenshtein("saturday", "sunday")}");
+            Console.WriteLine("\n");
+            //Console.WriteLine($"Costante: {DistanzaLevenshtein("rombo", "tromba")}");
+
+            //string g = "casa";
+            //Console.WriteLine($"{g[2]}");
+        }
+
+        public static int DistanzaLevenshtein(string s, string t)
+        {
+            //2.
+            //se parola ha 4 lettere la matrice avrà quindi 5 righe
+
+            s = s.ToLower();
+            t = t.ToLower();
+            int n = s.Length;
+            int m = t.Length;
+            
+            //2.1.
+            if(n==0)
+            {
+                //se n=0 su ha DL=m,stampare DL e terminare
+                //quindi stampo m
+                return m;
+            }
+
+            //2.2.
+            if (m==0)
+            {
+                //se n=0 su ha DL=m,stampare DL e terminare
+                //quindi stampo n
+                return n;
+            }
+
+            //3.
+            //es: se parola ha 4 lettere la matrice quindi 5 righe,quindi n+1
+            int[,] d = new int[n + 1, m + 1];
+
+            //3.1   inizializzo la prima riga con i valori da 0 a n
+            for (int i = 0; i <= n;i++)
+            {
+                d[i, 0] = i;
+            }
+
+            //3.2   inizializzo la prima colonna con i valori da 0 a n
+            for (int j = 0; j <= m;j++)
+            {
+                d[0, j] = j;
+            }
+
+            //4.    giro la matrice e faccio quel che devo
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int costo;
+                    //4.1   se t[j] = s[i] allora costo=0 oppure costo=1
+                    //dove t[] sarebbe il carattere della prima parola
+                    //dove s[] sarebbe il carattere della seconda parola
+                    if(t[j - 1] == s[i - 1])
+                    {
+                        costo = 0;
+                    }
+                    else
+                    {
+                        costo = 1;
+                    }
+
+                    //4.2
+                    //prendo il minimo valore fra questi
+                    int a = d[i - 1, j] + 1;
+                    int b = d[i, j - 1] + 1;
+                    int c = d[i - 1, j - 1] + costo;
+
+                    if (a <= b && a <= c)
+                    {
+                        d[i, j] = a;
+                    }
+                    else if (b <= c && b <= a)
+                    {
+                        d[i, j] = b;
+                    }
+                    else
+                    {
+                        d[i, j] = c;
+                    }
+
+                }
+            }
+
+            //stampa extra
+            //Console.WriteLine($"Parole prese:\n\t{s}\n\t{t}\n");
+            //Console.WriteLine("Stampo la matrice");
+            //for (int i = 0; i <=n; i++)
+            //{
+            //    Console.WriteLine("\n");
+            //    for (int j = 0; j <=m; j++)
+            //        Console.Write(d[i,j]);
+            //}
+
+            StampaMatrice(s, t, n, m, d);
+
+            Console.WriteLine("\n");
+            //5.
+            return d[n,m];
+        }
+
+        static void StampaMatrice(string s, string t, int n, int m, int[,] d)
+        {
+            //stampa extra
+            Console.WriteLine($"Parole prese:\n\t{s}\n\t{t}\n");
+            Console.WriteLine("Stampo la matrice");
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 0; j <= m; j++)
+                    Console.Write(d[i, j]);
+            }
+        }
+    }
+}
